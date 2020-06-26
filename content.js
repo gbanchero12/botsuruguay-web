1e67794a9decdc6e0d6beadc527137a71a46e8cc
= <gbanchero@hexa.com.uy>
2020-06-26 20:18:26 -0300

    rebase -1

--- content.js
+++ content.js
@@ -1,4 +1,140 @@
-const blog =
+const blog = {
+  "Titulo":"Algunos beneficios de utilizar chatbots con inteligencia artificial para tu empresa:",
+  "lista":[
+  "Ahorra tiempo y dinero",
+  "Chatbot WhatsApp",
+  "Potencia tus ofertas",
+  "Agiliza el proceso de cobro",
+  "Proporciona respuestas rápidas",
+  "Adaptable a cualquier negocio",
+  "Aumenta la confianza de tus ofertas",
+  "Genera datos para el análisis",
+  "Disminución de errores",
+  "Solicitud de datos"],
+  "listaBeneficios":{
+  "titulo":"Ahorra tiempo y dinero",
+  "desc":"Los bots permiten ahorrar dinero estando disponibles las 24 horas del día para su negocio. Dan respuestas certeras personalizadas con anterioridad, para cada casuística definida en el flujo de conversación. Al no necesitar un salario, crear un chatbot es una inversión que garantiza rendimientos.",
+  "titulo2":"Chatbot WhatsApp",
+  "desc2":"Puedes acceder a varias plataformas a través de chatbots, entre ellas WhatsApp. Esto permite no solo figurar en esta popular aplicación si no que gestionar tu audiencia de forma rápida y sencilla.",
+  "titulo3":"Potencia tus ofertas",
+  "desc3":"Permiten potenciar tus ofertas, diversificando el fujo establecido guías a tu cliente filtrando tus ofertas entre sus preferencias. En caso que el cliente necesite más detalles, el chatbot puede proporcionar las descripciones necesarias sobre tus productos o servicios.",
+  "titulo4":"Agiliza el proceso de cobro",
+  "desc4":"Esta tecnología permite conectarse con múltiples servicios externos, por ejemplo PayPal u otros métodos de pago. Gracias a esto permiten agilizar y simplificar el proceso de compra de un cliente, aumentando las posibilidades de venta.",
+  "titulo5":"Proporciona respuestas rápidas",
+  "desc5":"Una respuesta rápida le permite a los clientes resolver sus problemas ágilmente y da como resultado una experiencia positiva para el usuario. Si se realiza de manera adecuada, el chatbot le dará valor a la imagen de su empresa y aumentará la sensación de confianza en su marca.",
+  "titulo6":"Adaptable a cualquier negocio",
+  "desc6":"Esta tecnología es adaptable a cualquier negocio, puedes ver los ejemplos que tenemos publicados en nuestra web asociados a inmobiliarias, casas de préstamos financieros y soporte interno para empresas. Pronto publicaremos más ejemplos con los que hemos trabajado como e-commerce para minimercados, venta de alimentos, gimnasios y otros negocios locales.",
+  "titulo7":"Aumente la confianza",
+  "desc7":"La interacción se lleva a cabo en aplicaciones de mensajería populares como WhatsApp, Messenger, Telegram, etc., con una audiencia muy grande, alrededor de seis mil millones de usuarios según las estadísticas. Tendrá la oportunidad de comunicarse en un entorno confiable y altamente dirigido a escala global.",
+  "titulo8":"Genera datos para el análisis",
+  "desc8":"Con la continua interacción del 'tráfico en el chatbot accederás a un historial de conversación, generando una base de datos para analizar y perfeccionar las ofertas a tu audiencia. Podrás gestionar tu estrategia de marketing basándote en datos reales para mejorar la comunicación con el usuario final.'",
+  "titulo9":"Disminución de errores",
+  "desc9":"A nadie le gusta hacer el mismo trabajo una y otra vez durante un largo período de tiempo. En el caso de los seres humanos esto trae problemas, como estar propenso a equivocaciones. Los chatbots automatizan estas tareas que deben realizarse una y otra vez. También ofrecen la opción de dirigir el contacto hacia un agente, en caso que el usuario así lo prefiera. Esto ayuda a ahorrar tiempo y disminuir la cantidad de errores, aumentando la productividad en pocas semanas.",
+  "titulo10":"Solicitud de datos",
+  "desc10":"Está bien pedirles datos a los usuarios que compartan su nombre, edad, ubicación, etc. Esto permite proporcionar una forma de interacción personalizada en el futuro. Se puede solicitar una audiencia a futuro, para continuar la comunicación en otras plataformas si el chat no es conveniente en ese caso en particular.",
+  "titulo11":"Referencias:",
+  "desc11":'El articulo "Build Operate Transfer (BOT) Model in Software Outsourcing" de Saigontechnology.',
+  "desc12":"El artículo 'Top 5 Benefits Of Using Chatbots For Your Business' de Chatbotsmagazine"}
+  }
+
+  const features = [
+      {
+        color: "#304FFE",
+        headline: "Respuestas inmediatas",
+        text:
+          "BotsUruguay se encarga de crear respuestas con algoritmos de tecnología artificial, rápidas y eficientes según los requerimientos de nuestros clientes."
+      },
+      {
+        color: "#6200EA",
+        headline: "Mejora tus colocaciones",
+        text:
+        "Con BotsUruguay podrás cubrir la demanda de tus clientes. Estando disponible 24/7 todos los días del año, mejorando tus colocaciones en pocas semanas."
+      },
+      {
+        color: "#00C853",
+        headline: "Integraciones",
+        text:
+          "Tenemos integración con Whatsapp, puedes obtener tu numero personalizado y el bot se encarga de responder por vos. Consulta por integración con Facebook, mensajes SMS, entre otros."
+      },
+      {
+        color: "#d50000",
+        headline: "Acceso a tus datos en tiempo real",
+        text:
+          "La información se almacena en una base que podrás consultar en tiempo real, desde un administrador. "
+      },
+      {
+        color: "#64DD17",
+        headline: "Productividad",
+        text:
+          "BotsUruguay es capaz de dar respuestas cognitivas certeras, mejorando la producción del equipo de trabajo."
+      },
+      {
+        color: "#0091EA",
+        headline: "Precios",
+        text:
+          "Somos una software factory especializada en la creación de bots customizados, por lo que manejamos los precios mas competitivos del mercado."
+      },
+      {
+        color: "#C51162",
+        headline: "Soporte",
+        text:
+        "En BotsUruguay estamos atentos a las solicitudes de nuestros clientes las 24 horas del día. "
+      },
+      {
+        color: "#DD2C00",
+        headline: "Desarrollo personalizado",
+        text:
+          "Consulta por desarrollo personalizado para integrarnos a tu sistema de información."
+      },
+    
+    ];
+    
+    
+    const bots = {
+        Tom: {
+            nombre:"Tom",
+            fechaLanzamiento:"Mayo 02, 2020",
+            breveDescripcion:"Habilidad: Simpatía",
+            textoPrincipal:"Tom trabaja en Bots Uruguay arduamente desde mayo de 2020, se dedica"
+            + " a darle la bienvenida a los clientes de BotsUruguay.com y contarle los servicios que ofrece la empresa."
+            
+        },
+        Alberth: {
+            nombre:"Alberth",
+            fechaLanzamiento:"Enero 15, 2020",
+            breveDescripcion:"Habilidad: Finanzas",
+            textoPrincipal:"Alberth trabaja en casas de préstamos financieros arduamente desde enero de 2020, se dedica"
+            + " a darle la bienvenida a los clientes y ofrecer servicios financieros. Puede calcular valores de cuotas y realizar consultas al clearing de informes, entre otras funcionalidades."
+            
+        },
+        Iliana: {
+            nombre:"Iliana",
+            fechaLanzamiento:"Febrero 10, 2020",
+            breveDescripcion:"Habilidad: Negocios inmobiliarios",
+            textoPrincipal:"Iliana existe desde febrero del 2020, sus habilidades le permiten realizar una serie de preguntas,"
+            + " que permiten filtrar inmuebles en la web según los gustos y las posibilidades de los interesados."
+            + " También responde preguntas frecuentes, entre otras funcionalidades."
+        },
+        Alexa: {
+            nombre:"Alexa",
+            fechaLanzamiento:"Marzo 20, 2020",
+            breveDescripcion:"Habilidad: Gestión de soporte interno",
+            textoPrincipal:"Alexa existe desde marzo del 2020, sus habilidades le permiten gestionar los tickets de soporte interno de las empresas."
+            + " Se conecta con aplicaciones permitiéndole a los empleados gestionar sus solicitudes de forma ordenada."
+            + " También responde preguntas frecuentes, entre otras funcionalidades."
+      },
+    }
+
+    
+
+
+  module.exports = {
+      blog,
+      bots,
+      features
+  }
+
+/*const blog =
   [{
     title: "Beneficios de utilizar bots para tu empresa",
     id: 1,
@@ -186,4 +322,4 @@ module.exports = {
   blog,
   bots,
   features
-}
+}*/
